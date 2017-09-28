583061e55aca370039e3473e7dc5d3259c8d5050
Rajal Kumar <rajalkumar13@hotmail.com>
2017-09-29 00:28:11 +1300

    seeds sorted

--- migrations/20160802144759_users.js
+++ migrations/20160802144759_users.js
@@ -2,7 +2,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments('id').primary()
     table.string('name')
-    table.string('email')
+    table.string('github')
   })
 }
 
--- migrations/20170928234028_beers.js
+++ migrations/20170928234028_beers.js
@@ -1,8 +1,9 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('beers', function (table) {
     table.increments('beer_id').primary()
-    table.string('beer_name')
-    table.string('alc_lvl')
+    table.string('beer_brand')
+    table.string('beer_type')
+    table.string('beer_image')
     table.string('image')
   })
 }
