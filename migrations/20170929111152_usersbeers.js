89224d442544e67ba8a1d7c850de5b3946cbff4d
albie-feary <laurabeth.feary@gmail.com>
2017-09-29 12:15:18 +1300

    sort new tables

--- migrations/20170929111152_usersbeers.js
+++ migrations/20170929111152_usersbeers.js
@@ -1,9 +1,10 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('usersbeers', function (table) {
-    table.increments('usertriesbeer').primary()
-    table.string('user_id').references()
-    table.string('beer_id').references()
+    table.integer('usertriesbeer')
+    table.integer('userlikesbeer')
+    table.string('user_id').references('users.user_id')
+    table.string('beer_id').references('beers.beer_id')
  
 };
 
--- seeds/usersbeers.js
+++ seeds/usersbeers.js
@@ -1,49 +1,151 @@
 exports.seed = function (knex, Promise) {
-  return knex('beers').del()
+  return knex('usersbeers').del()
     .then(function () {
       return Promise.all([
-        knex('beers').insert({
+        knex('usersbeers').insert({
+          user_id: 1001,
           beer_id: 99901,
-          beer_brand: 'Panhead',
-          beer_type: 'Supercharger APA',
-          beer_image: 'http://beerhealer.com/wp-content/uploads/2016/01/10727481_745451195529720_1542626673_n.jpg',
-          image: 'http://brewersguild.org.nz/sites/default/files/imagecache/Logo_Main/Panhead.jpg'
+          usertriesbeer: false,
+          userlikesbeer: false
         }),
-        knex('beers').insert({
+        knex('usersbeers').insert({
+          user_id: 1001,
           beer_id: 99902,
-          beer_brand: 'Tuatara',
-          beer_type: 'Double-Trouble Extra-Dimensional APA',
-          beer_image: 'https://i.ytimg.com/vi/4oXDWQnq5ps/maxresdefault.jpg',
-          image: 'https://main-cdn.grabone.co.nz/goimage/440x267/ilkf4xpxp.jpg'
+          usertriesbeer: true,
+          userlikesbeer: false
         }),
-        knex('beers').insert({
+        knex('usersbeers').insert({
+          user_id: 1001,
           beer_id: 99903,
-          beer_brand: 'Garage Project',
-          beer_type: 'PillsnThrills Ale',
-          beer_image: 'http://www.craftbeeronline.co.nz/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/g/a/garage_project_pils_n_thrills_can.jpg',
-          image: 'https://yt3.ggpht.com/-s1vvPGecMQg/AAAAAAAAAAI/AAAAAAAAAAA/VRwcLH4ykQ8/s900-c-k-no-mo-rj-c0xffffff/photo.jpg'
+          usertriesbeer: true,
+          userlikesbeer: true
         }),
-        knex('beers').insert({
+        knex('usersbeers').insert({
+          user_id: 1001,
           beer_id: 99904,
-          beer_brand: 'Monteiths',
-          beer_type: 'RIPA Red IPA',
-          beer_image: 'https://thehitchhikersgubeer_idetobeer.files.wordpress.com/2016/05/img_0292_crop.jpg?w=640',
-          image: 'http://www.voxy.co.nz/files/imagecache/news_item_image/files/Monteiths%20LOGO.jpg'
+          usertriesbeer: true,
+          userlikesbeer: true
         }),
-        knex('beers').insert({
+        knex('usersbeers').insert({
+          user_id: 1001,
           beer_id: 99905,
-          beer_brand: 'Epic',
-          beer_type: 'Armageddon IPA',
-          beer_image: 'http://www.craftbeeronline.co.nz/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/e/p/epic_armageddon.jpg',
-          image: 'http://www.fallingapple.org.nz/wp-content/uploads/2014/11/epic-logo-copy.jpg'
+          usertriesbeer: true,
+          userlikesbeer: false
         }),
-        knex('beers').insert({
+        knex('usersbeers').insert({
+          user_id: 1001,
           beer_id: 99906,
-          beer_brand: 'Mata',
-          beer_type: 'Wai-iti Waka NZ Pale Ale',
-          beer_image: 'http://www.craftbeeronline.co.nz/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/a/mata_wai_iti_nz_pale_ale.jpg',
-          image: 'https://www.soba.org.nz/wordpress/wp-content/uploads/2012/06/Mata.jpg'
-        })
-      ])
-    })
-}
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+
+        knex('usersbeers').insert({
+          user_id: 1002,
+          beer_id: 99901,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1002,
+          beer_id: 99902,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1002,
+          beer_id: 99903,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1002,
+          beer_id: 99904,
+          usertriesbeer: false,
+          userlikesbeer: false
+        }),
+        knex('usersbeers').insert({
+          user_id: 1002,
+          beer_id: 99905,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1002,
+          beer_id: 99906,
+          usertriesbeer: false,
+          userlikesbeer: false
+        }),
+
+        knex('usersbeers').insert({
+          user_id: 1003,
+          beer_id: 99901,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1003,
+          beer_id: 99902,
+          usertriesbeer: true,
+          userlikesbeer: false
+        }),
+        knex('usersbeers').insert({
+          user_id: 1003,
+          beer_id: 99903,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1003,
+          beer_id: 99904,
+          usertriesbeer: true,
+          userlikesbeer: false
+        }),
+        knex('usersbeers').insert({
+          user_id: 1003,
+          beer_id: 99905,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1003,
+          beer_id: 99906,
+          usertriesbeer: false,
+          userlikesbeer: false
+        }),
+
+        knex('usersbeers').insert({
+          user_id: 1004,
+          beer_id: 99901,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1004,
+          beer_id: 99902,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1004,
+          beer_id: 99903,
+          usertriesbeer: false,
+          userlikesbeer: false
+        }),
+        knex('usersbeers').insert({
+          user_id: 1004,
+          beer_id: 99904,
+          usertriesbeer: true,
+          userlikesbeer: true
+        }),
+        knex('usersbeers').insert({
+          user_id: 1004,
+          beer_id: 99905,
+          usertriesbeer: false,
+          userlikesbeer: false
+        }),
+        knex('usersbeers').insert({
+          user_id: 1004,
+          beer_id: 99906,
+          usertriesbeer: true,
+          userlikesbeer: false
+        }),
