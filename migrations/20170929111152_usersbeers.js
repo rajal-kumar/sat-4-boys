cb31f853a9adac9719573d1acd9c15805ff82b32
Rajal Kumar <rajalkumar13@hotmail.com>
2017-09-29 16:22:58 +1300

    fixed

--- migrations/20170929111152_usersbeers.js
+++ migrations/20170929111152_usersbeers.js
@@ -5,8 +5,8 @@ exports.up = function(knex, Promise) {
     table.integer('userlikesbeer')
     table.string('user_id').references('users.user_id')
     table.string('beer_id').references('beers.beer_id')
- 
-};
+  });
+}
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('usersbeers')
