4e4d9d0d3e12385827b1b3768d16bddab349d939
eda013 <poukupenga@gmail.com>
2017-10-02 13:49:41 +1300

    sort beer route

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,8 @@ router.get('/', function (req, res) {
 })
 
 router.get('/beers', function (req, res) {
-  db.getBeers(typeof req.app.get('connection'))
-    console.log(req.app.get('connection'))
-    .then(function (beers) {
+  db.getBeers(req.app.get('connection'))
+      .then(function (beers) {
       res.render('beers', {
         beers: beers,
         // users: users
